bea83256486eb2db83f7ddf5b410597c59845ed7
BT_1N3_25 <f>
2018-12-18 11:34:32 -0500

    f

--- src/Runner.java
+++ src/Runner.java
@@ -8,8 +8,24 @@ public class Runner {
                 {17, 11, 16, 14}
         };
 
+        /*
         Position partA = Successors.findPosition(7, coolArray);
         System.out.println("Row of 7: " + partA.r);
         System.out.println("Column of 7: " + partA.c);
+        */
+
+        Position[][] partB = Successors.getSuccessorArray(coolArray);
+
+        for (int i = 0; i < partB.length; i ++) {
+            for (int j = 0; j < partB[i].length; j ++) {
+                if (partB[i][j] != null) {
+                    System.out.print(("(" + partB[i][j].r + ", " + partB[i][j].c + ")") + "; ");
+                }
+                else {
+                    System.out.print("null" + "; ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
--- src/Successors.java
+++ src/Successors.java
@@ -4,17 +4,31 @@ public class Successors {
      * @param num The number that we are trying to find the position of
      * @param intArr Since this is a static method in a separate class, we will need to pass in the array we want
      *               to find the position of num from.
-     * @return Position object initialized with the row and column that we found.
+     * @return Position object initialized with the row and column that we found. If the position cannot be found,
+     * return null.
      */
     public static Position findPosition(int num, int[][] intArr) {
-        int row = 0, column = 0;
         for (int i = 0; i < intArr.length; i ++) {
             for (int j = 0; j < intArr[i].length; j ++) {
                 if (num == intArr[i][j]) {
-                    row = i; column = j;
+                    return new Position(i, j);
                 }
             }
         }
-        return new Position(row, column);
+        return null;
+    }
+
+    /**
+     * @param intArr The array that we will be working with.
+     * @return A 2D array containing Position objects of the successive positions of each cell in the 2D array.
+     */
+    public static Position[][] getSuccessorArray(int[][] intArr) {
+        Position[][] output = new Position[intArr.length][intArr[0].length];
+        for (int i = 0; i < intArr.length; i ++) {
+            for (int j = 0; j < intArr[i].length; j ++) {
+                output[i][j] = findPosition((intArr[i][j] + 1), intArr);
+            }
+        }
+        return output;
     }
 }
